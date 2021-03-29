423df9263257cf5c492d199097c6433f56b937e7
jstoone <jstoone@drk.sh>
2021-03-29 23:08:30 +0200

    Needed proof, that it actually works.

--- app/Http/Livewire/Exponentiles.php
+++ app/Http/Livewire/Exponentiles.php
@@ -2,10 +2,64 @@
 
 namespace App\Http\Livewire;
 
+use Exponentiles\Engine\Engine;
+use Exponentiles\Engine\Tile;
+use Illuminate\Support\Facades\Cache;
 use Livewire\Component;
 
 class Exponentiles extends Component
 {
+    /**
+     * @var array<int, int>
+     */
+    public array $tiles;
+
+    private Engine $engine;
+
+    public function mount()
+    {
+        $this->engine = new Engine();
+
+        if (Cache::missing('game')) {
+            $this->engine->start();
+
+            Cache::put('game', $this->engine->grid->toArray());
+        }
+
+        $this->engine->grid->fromArray(
+            Cache::get('game')
+        );
+
+        $this->tiles = Cache::get('game');
+    }
+
+    public function steer(string $direction)
+    {
+        $this->engine = new Engine();
+
+        $this->engine->grid->fromArray(
+            Cache::get('game')
+        );
+
+        $this->engine->steer($direction);
+        $this->engine->addTile();
+
+        Cache::put('game', $this->engine->grid->toArray());
+
+        $this->tiles = Cache::get('game');
+    }
+
+    public function newGame()
+    {
+        $this->engine = new Engine();
+
+        $this->engine->start();
+
+        Cache::put('game', $this->engine->grid->toArray());
+
+        $this->tiles = Cache::get('game');
+    }
+
     public function render()
     {
         return view('livewire.exponentiles');
--- resources/views/livewire/exponentiles.blade.php
+++ resources/views/livewire/exponentiles.blade.php
@@ -1,9 +1,31 @@
 <div>
-    <div class="h-64 grid grid-cols-3 grid-flow-row gap-4 text-gray-300">
-        @foreach(range(1, 9) as $index)
-            <div class="bg-blue-500 rounded-md flex items-center justify-center text-2xl font-extrabold">
-                {{ $index }}
-            </div>
-        @endforeach
+  <div class="w-1/2 mx-auto">
+    <div class="h-96 grid grid-cols-4 grid-flow-row gap-4 text-black text-lg font-mono px-8 pt-8">
+      @foreach($this->tiles as $tile)
+        <div class="bg-blue-300 rounded-md flex items-center justify-center text-2xl font-bold">
+          {{ $tile }}
+        </div>
+      @endforeach
     </div>
+  </div>
+  <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 text-center">
+    @foreach (['WEST', 'NORTH', 'SOUTH', 'EAST'] as $direction)
+      <x-jet-button type="button"
+                    wire:loading.attr="disabled"
+                    wire:click="steer('{{$direction}}')"
+      >
+        {{ __($direction) }}
+      </x-jet-button>
+    @endforeach
+
+    <x-jet-section-border />
+
+    <x-jet-danger-button type="button"
+                         wire:loading.attr="disabled"
+                         wire:click="newGame()"
+    >
+      {{ __('New game') }}
+    </x-jet-danger-button>
+  </div>
+
 </div>
